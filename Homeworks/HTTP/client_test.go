15aca0a573fd814f115566fd9fcbbc71eca2cf03
Maria <64471671+MariaMayy@users.noreply.github.com>
2021-07-20 20:12:49 +0300

    Coverage 90.1%

--- Homeworks/HTTP/client_test.go
+++ Homeworks/HTTP/client_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -237,6 +238,153 @@ func TestFatalError(t *testing.T) {
 		t.Errorf("Empty error, bro")
 	} else if err.Error() != "SearchServer fatal error" {
 		t.Errorf("Invalid error: %v", err.Error())
+	}
+
+}
+
+func TestUnpackResultErrorFail(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintln(w, "Cant unpack result json!")
+		}))
+	defer ts.Close()
+	sClient := SearchClient{AccessToken, ts.URL}
+
+	sc := SearchRequest{
+		Limit:      5,
+		Offset:     1,
+		Query:      "",
+		OrderField: "",
+		OrderBy:    OrderByAsc,
+	}
+	_, err := sClient.FindUsers(sc)
+	if err == nil {
+		t.Errorf("Empty error, bro")
+	} else if !strings.Contains(err.Error(), "cant unpack result json") {
+		t.Errorf("Invalid error: %v", err.Error())
+	}
+
+}
+
+func TestFindName(t *testing.T) {
+	ts := startTServer(AccessToken)
+	defer ts.Close()
+
+	sc := SearchRequest{
+		Limit:      1,
+		Offset:     0,
+		Query:      "Boyd",
+		OrderField: "",
+		OrderBy:    OrderByDesc,
+	}
+	find, _ := ts.search.FindUsers(sc)
+	if find.Users[0].Name == "" {
+		t.Errorf("Invalid user: %s", find.Users[0].Name)
+		return
+	}
+
+	if len(find.Users) != 1 {
+		t.Errorf("Invalid count of users: %d", len(find.Users))
+		return
+	}
+
+}
+
+func TestCorrectRequestWorksId(t *testing.T) {
+	ts := startTServer(AccessToken)
+	defer ts.Close()
+
+	searchRequest := SearchRequest{
+		Limit:      5,
+		Offset:     0,
+		OrderField: "Id",
+		OrderBy:    OrderByDesc,
+	}
+	find, _ := ts.search.FindUsers(searchRequest)
+
+	if !find.NextPage {
+		t.Error("NextPage is invalid")
+	}
+
+	if len(find.Users) != 5 {
+		t.Error("Wrong count of users:c")
+	}
+}
+
+func TestCorrectRequestWorksName(t *testing.T) {
+	ts := startTServer(AccessToken)
+	defer ts.Close()
+
+	searchRequest := SearchRequest{
+		Limit:      5,
+		Offset:     0,
+		OrderField: "Name",
+		OrderBy:    OrderByDesc,
+	}
+	find, _ := ts.search.FindUsers(searchRequest)
+
+	if !find.NextPage {
+		t.Error("NextPage is invalid")
+	}
+
+	if len(find.Users) != 5 {
+		t.Error("Wrong count of users:c")
+	}
+}
+
+func TestCorrectRequestWorksAge(t *testing.T) {
+	ts := startTServer(AccessToken)
+	defer ts.Close()
+
+	searchRequest := SearchRequest{
+		Limit:      5,
+		Offset:     0,
+		OrderField: "Age",
+		OrderBy:    OrderByDesc,
+	}
+	find, _ := ts.search.FindUsers(searchRequest)
+
+	if !find.NextPage {
+		t.Error("NextPage is invalid")
+	}
+
+	if len(find.Users) != 5 {
+		t.Error("Wrong count of users:c")
+	}
+}
+func TestNotFound(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+	defer ts.Close()
+	cases := []SearchRequest{
+		{
+			Limit:      8,
+			Offset:     100,
+			Query:      "",
+			OrderField: "",
+			OrderBy:    1,
+		},
+		{
+			Limit:      8,
+			Offset:     10,
+			Query:      "WOW",
+			OrderField: "",
+			OrderBy:    1,
+		},
+		{
+			Limit:      8,
+			Offset:     1,
+			Query:      "",
+			OrderField: "",
+			OrderBy:    1,
+		},
+	}
+
+	for _, item := range cases {
+		sClient := SearchClient{Token, ts.URL}
+		sr, err := sClient.FindUsers(item)
+		if sr != nil {
+			fmt.Println(err)
+		}
 	}
 
 }
